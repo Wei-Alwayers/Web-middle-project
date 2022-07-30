d01e1ad945dc711c89f3bf65883afbdfa9e28273
Wei-Alwayers <weihaomng2002@icloud.com>
2022-07-30 11:45:18 +0800

    update control

--- static/js/game_object/player/base.js
+++ static/js/game_object/player/base.js
@@ -32,6 +32,45 @@ class Player extends GameObject {
 
     }
 
+    update_control() {
+        let w, a, d, space;
+        if (this.id === 0) {
+            w = this.pressed_keys.has('w');
+            a = this.pressed_keys.has('a');
+            d = this.pressed_keys.has('d');
+            space = this.pressed_keys.has(' ');
+        } else {
+            w = this.pressed_keys.has('ArrowUp');
+            a = this.pressed_keys.has('ArrowLeft');
+            d = this.pressed_keys.has('ArrowRight');
+            space = this.pressed_keys.has('Enter');
+        }
+
+        if (this.status === 0 || this.status === 1) {
+            // 跳跃的条件
+            if (w) {
+                if (d) {
+                    this.vx = this.speedx;
+                } else if (a) {
+                    this.vx = -this.speedx;
+                } else {
+                    this.vx = 0;
+                }
+                this.vy = -this.speedy;
+                this.status = 3;
+            } else if (d) {
+                this.vx = this.speedx;
+                this.status = 1;
+            } else if (a) {
+                this.vx = -this.speedx;
+                this.status = 1;
+            } else {
+                this.vx = 0;
+                this.status = 0;
+            }
+        }
+    }
+
     update_move() {
         this.vy += this.gravity;
         this.x += this.vx * this.timedelta / 1000;
@@ -40,10 +79,19 @@ class Player extends GameObject {
         if (this.y > 450) {
             this.y = 450;
             this.vy = 0;
+            this.status = 0;
         }
+        // 防止玩家走出地图
+        if (this.x < 0) {
+            this.x = 0;
+        } else if (this.x + this.width > this.root.game_map.$canvas.width()) {
+            this.x = this.root.game_map.$canvas.width() - this.width;
+        }
+
     }
 
     update() {
+        this.update_control();
         this.update_move();
 
         this.render();
